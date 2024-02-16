254a2f9e96aa3fb6757960b751b379f1979db120
Carlo Tamayo <Cejota708@gmail.com>
2024-02-16 15:44:37 -0600

    02 2° app career - career model

---
+++ seani/career/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Career
+
+class CareerAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'level']
+    list_filter=['level']
+
+admin.site.register(Career, CareerAdmin)
--- seani/library/models.py
+++ seani/library/models.py
@@ -5,4 +5,8 @@ class Meta:
     verbose_name='modulo'
     verbose_name_plural='modulos'
 
+class Meta:
+    verbose_name='pregunta'
+    verbose_name_plural='preguntas'
+
 # Create your models here.
